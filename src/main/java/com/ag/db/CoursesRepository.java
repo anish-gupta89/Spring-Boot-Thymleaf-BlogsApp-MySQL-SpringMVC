cf4caa9fd749d97e15ab92a64e0e005c331b1bcb
Anish Gupta <47443012+anish-gupta89@users.noreply.github.com>
2023-05-08 12:22:53 +0530

    Deleted file
    Deleted file wrong merger

--- src/main/java/com/ag/db/CoursesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ag.db;
-
-import java.io.Serializable;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CoursesRepository extends CrudRepository<CoursesEntity, Serializable>{
-
-}
